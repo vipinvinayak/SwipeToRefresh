d882b834e7809b618bb35afeb2f34e6f4f6a9c39
vipin <vipinvinayak01@gmail.com>
2017-06-23 00:45:54 +0530

    change in Adapter

---
+++ app/src/main/java/com/example/vipin/swipetorefresh/helper/SwipeListAdapter.java
@@ -0,0 +1,65 @@
+package com.example.vipin.swipetorefresh.helper;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.vipin.swipetorefresh.R;
+
+import java.util.List;
+
+/**
+ * Created by vipin on 6/22/2017.
+ */
+
+public class SwipeListAdapter extends BaseAdapter {
+    private Activity activity;
+    private LayoutInflater inflater;
+    private List<Movie> movieList;
+    private String[] bgColors;
+
+    public SwipeListAdapter(Activity activity, List<Movie> movieList)
+    {
+        this.activity = activity;
+        this.movieList = movieList;
+        bgColors = activity.getApplicationContext().getResources()
+                .getStringArray(R.array.movie_serial_bg);
+    }
+
+    @Override
+    public int getCount() {
+        return movieList.size();
+    }
+
+    @Override
+    public Object getItem(int location) {
+        return movieList.get(location);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (inflater == null)
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView==null)
+            convertView = inflater.inflate(R.layout.list_row, null);
+
+        TextView serial = (TextView) convertView.findViewById(R.id.serial);
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+
+        String color = bgColors[position % bgColors.length];
+        serial.setBackgroundColor(Color.parseColor(color));
+        return convertView;
+    }
+}
